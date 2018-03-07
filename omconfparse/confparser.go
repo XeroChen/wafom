d24d3b0c773a88af2968e11b381868901f6a2980
陈曦 <chenxi@chenxideMacBook-Pro.local>
2018-03-07 15:18:42 +0800

    yaml parser

--- omconfparse/confparser.go
+++ omconfparse/confparser.go
@@ -1,8 +1,10 @@
 package omconfparse
 
 import (
-	"github.com/go-yaml/yaml"
+	//"github.com/go-yaml/yaml"
 	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
 )
 
 // ParseYamlFile parse yaml file to map[interface{}]interface{}
@@ -14,4 +16,16 @@ func ParseYamlFile(filepath string) (result map[interface{}]interface{}, err err
 	conf := make(map[interface{}]interface{})
 	err = yaml.Unmarshal(content, &conf)
 	return conf, err
-} 
+}
+
+func ParseConf(confdata map[interface{}]interface{}) {
+	for k, v := range confdata {
+		if k == "webapps" {
+			ParseSite(v)
+		}
+	}
+}
+
+func ParseSite(sitedata map[interface{}]interface{}) {
+
+}
--- omdata/site.go
+++ omdata/site.go
@@ -1,30 +1,30 @@
 package wafsite
 
-type site struct {
+type Site struct {
 	index    uint32
 	sitename string /* name */
 	vhindex  uint32
 	spindex  uint32
 }
 
-type serverpool struct {
+type Serverpool struct {
 	index uint32
 	ip    uint64 /* ip */
 	port  uint32 /* port */
 }
 
-type vhost struct {
+type Vhost struct {
 	index   uint32
 	gwindex uint32
 }
 
-type vhmatchtable struct {
+type Vhmatchtable struct {
 	vhindex uint32 /* key1 */
 	port    uint16 /* frontend_port */
 	domain  string /* domain_list[] */
 }
 
-type gateway struct {
+type Gateway struct {
 	index  uint32
 	gwtype uint32   /* 0:ip 1:bridge REF:deploy:transparent */
 	gwip   uint64   /* frontend */
